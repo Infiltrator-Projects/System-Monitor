38bd8cd0147546fab537a8d89fd135c72569e553
Shannon Smith <infiltratr@yandex.com>
2026-09-21 11:09:12 +1000

    Normalize System Monitor project start year

--- src/graph.c
+++ src/graph.c
@@ -7,7 +7,7 @@
  * SysMonTask graphs while remaining a native C implementation.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2000-2026 Shannon Smith
+ * @copyright Copyright (c) 2016-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "graph.h"
