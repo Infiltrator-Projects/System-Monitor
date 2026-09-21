af665b174e799e5233aa1b85e35cb3c3eb7ad0fb
Shannon Smith <infiltratr@yandex.com>
2026-09-21 11:09:30 +1000

    Normalize System Monitor project start year

--- src/refresh_policy.c
+++ src/refresh_policy.c
@@ -4,7 +4,7 @@
  * @brief Pure refresh-policy implementation shared by backend and UI code.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2000-2026 Shannon Smith
+ * @copyright Copyright (c) 2016-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "refresh_policy.h"
