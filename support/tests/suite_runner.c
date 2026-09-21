d1784f8f6b105340b2411a713ebcc40200ba15de
Shannon Smith <infiltratr@yandex.com>
2026-09-21 11:19:56 +1000

    Keep smoke suite entry points internal

--- support/tests/suite_runner.c
+++ support/tests/suite_runner.c
@@ -21,13 +21,13 @@ typedef struct {
     LsmSmokeCaseFunction function;
 } LsmSmokeCase;
 
-#define LSM_CASE(symbol) { #symbol, lsm_case_##symbol }
+#define LSM_CASE(symbol) { #symbol, smoke_case_##symbol }
 
 #if defined(LSM_SUITE_CORE)
-int lsm_case_atomic_file(void);
-int lsm_case_duration_format(void);
-int lsm_case_common(void);
-int lsm_case_project_info(void);
+int smoke_case_atomic_file(void);
+int smoke_case_duration_format(void);
+int smoke_case_common(void);
+int smoke_case_project_info(void);
 static const char suite_name[] = "core";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(atomic_file),
@@ -36,19 +36,19 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(project_info),
 };
 #elif defined(LSM_SUITE_BACKEND)
-int lsm_case_backend(void);
-int lsm_case_monitor_platform(void);
+int smoke_case_backend(void);
+int smoke_case_monitor_platform(void);
 static const char suite_name[] = "backend";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(backend),
     LSM_CASE(monitor_platform),
 };
 #elif defined(LSM_SUITE_PERIPHERAL)
-int lsm_case_bluetooth_battery(void);
-int lsm_case_bluetooth_traffic(void);
-int lsm_case_linux_capability(void);
-int lsm_case_logitech_hidpp(void);
-int lsm_case_wifi_metadata(void);
+int smoke_case_bluetooth_battery(void);
+int smoke_case_bluetooth_traffic(void);
+int smoke_case_linux_capability(void);
+int smoke_case_logitech_hidpp(void);
+int smoke_case_wifi_metadata(void);
 static const char suite_name[] = "peripheral";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(bluetooth_battery),
@@ -58,15 +58,15 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(wifi_metadata),
 };
 #elif defined(LSM_SUITE_METRICS)
-int lsm_case_cpu_accounting(void);
-int lsm_case_disk_accounting(void);
-int lsm_case_memory_accounting(void);
-int lsm_case_pressure(void);
-int lsm_case_cpu_direct(void);
-int lsm_case_quality_policy(void);
-int lsm_case_sample_history(void);
-int lsm_case_gpu_metrics(void);
-int lsm_case_performance_navigation(void);
+int smoke_case_cpu_accounting(void);
+int smoke_case_disk_accounting(void);
+int smoke_case_memory_accounting(void);
+int smoke_case_pressure(void);
+int smoke_case_cpu_direct(void);
+int smoke_case_quality_policy(void);
+int smoke_case_sample_history(void);
+int smoke_case_gpu_metrics(void);
+int smoke_case_performance_navigation(void);
 static const char suite_name[] = "metrics";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(cpu_accounting),
@@ -80,12 +80,12 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(performance_navigation),
 };
 #elif defined(LSM_SUITE_STORAGE)
-int lsm_case_mountinfo(void);
-int lsm_case_storage_metadata(void);
-int lsm_case_filesystem_inventory(void);
-int lsm_case_bundled_pci(void);
-int lsm_case_smbios_memory(void);
-int lsm_case_system_sources(void);
+int smoke_case_mountinfo(void);
+int smoke_case_storage_metadata(void);
+int smoke_case_filesystem_inventory(void);
+int smoke_case_bundled_pci(void);
+int smoke_case_smbios_memory(void);
+int smoke_case_system_sources(void);
 static const char suite_name[] = "storage";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(mountinfo),
@@ -96,12 +96,12 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(system_sources),
 };
 #elif defined(LSM_SUITE_PROCESS)
-int lsm_case_process_model(void);
-int lsm_case_process_grouping(void);
-int lsm_case_process_gpu(void);
-int lsm_case_process_inspection(void);
-int lsm_case_process_management(void);
-int lsm_case_efficiency(void);
+int smoke_case_process_model(void);
+int smoke_case_process_grouping(void);
+int smoke_case_process_gpu(void);
+int smoke_case_process_inspection(void);
+int smoke_case_process_management(void);
+int smoke_case_efficiency(void);
 static const char suite_name[] = "process";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(process_model),
@@ -112,11 +112,11 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(efficiency),
 };
 #elif defined(LSM_SUITE_UI)
-int lsm_case_dbus_models(void);
-int lsm_case_preferences(void);
-int lsm_case_startup(void);
-int lsm_case_ui_update(void);
-int lsm_case_task_manager_layout(void);
+int smoke_case_dbus_models(void);
+int smoke_case_preferences(void);
+int smoke_case_startup(void);
+int smoke_case_ui_update(void);
+int smoke_case_task_manager_layout(void);
 static const char suite_name[] = "UI/preferences";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(dbus_models),
@@ -126,9 +126,9 @@ static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(task_manager_layout),
 };
 #elif defined(LSM_SUITE_ACCELERATOR)
-int lsm_case_hardware_topology(void);
-int lsm_case_intel_gpu(void);
-int lsm_case_npu_telemetry(void);
+int smoke_case_hardware_topology(void);
+int smoke_case_intel_gpu(void);
+int smoke_case_npu_telemetry(void);
 static const char suite_name[] = "accelerator";
 static const LsmSmokeCase suite_cases[] = {
     LSM_CASE(hardware_topology),
