4de4fc02e063cfeee538cb7f43a020605556209c
Shannon Smith <infiltratr@yandex.com>
2026-09-21 11:19:59 +1000

    Keep smoke suite entry points internal

--- support/tests/suite_cases.h
+++ support/tests/suite_cases.h
@@ -1,46 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * @file suite_cases.h
+ * @brief Internal entry points for consolidated regression-suite cases.
+ *
+ * @author Shannon Smith
+ * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @license GPL-3.0-or-later
+ */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_SUITE_CASES_H
 #define INFILTRATOR_SYSTEM_MONITOR_SUITE_CASES_H
 
-int lsm_case_atomic_file(void);
-int lsm_case_duration_format(void);
-int lsm_case_common(void);
-int lsm_case_project_info(void);
-int lsm_case_backend(void);
-int lsm_case_monitor_platform(void);
-int lsm_case_bluetooth_battery(void);
-int lsm_case_bluetooth_traffic(void);
-int lsm_case_linux_capability(void);
-int lsm_case_logitech_hidpp(void);
-int lsm_case_wifi_metadata(void);
-int lsm_case_cpu_accounting(void);
-int lsm_case_disk_accounting(void);
-int lsm_case_memory_accounting(void);
-int lsm_case_pressure(void);
-int lsm_case_cpu_direct(void);
-int lsm_case_quality_policy(void);
-int lsm_case_sample_history(void);
-int lsm_case_gpu_metrics(void);
-int lsm_case_performance_navigation(void);
-int lsm_case_mountinfo(void);
-int lsm_case_storage_metadata(void);
-int lsm_case_filesystem_inventory(void);
-int lsm_case_bundled_pci(void);
-int lsm_case_smbios_memory(void);
-int lsm_case_system_sources(void);
-int lsm_case_process_model(void);
-int lsm_case_process_grouping(void);
-int lsm_case_process_gpu(void);
-int lsm_case_process_inspection(void);
-int lsm_case_process_management(void);
-int lsm_case_efficiency(void);
-int lsm_case_dbus_models(void);
-int lsm_case_preferences(void);
-int lsm_case_startup(void);
-int lsm_case_ui_update(void);
-int lsm_case_task_manager_layout(void);
-int lsm_case_hardware_topology(void);
-int lsm_case_intel_gpu(void);
-int lsm_case_npu_telemetry(void);
+int smoke_case_atomic_file(void);
+int smoke_case_duration_format(void);
+int smoke_case_common(void);
+int smoke_case_project_info(void);
+int smoke_case_backend(void);
+int smoke_case_monitor_platform(void);
+int smoke_case_bluetooth_battery(void);
+int smoke_case_bluetooth_traffic(void);
+int smoke_case_linux_capability(void);
+int smoke_case_logitech_hidpp(void);
+int smoke_case_wifi_metadata(void);
+int smoke_case_cpu_accounting(void);
+int smoke_case_disk_accounting(void);
+int smoke_case_memory_accounting(void);
+int smoke_case_pressure(void);
+int smoke_case_cpu_direct(void);
+int smoke_case_quality_policy(void);
+int smoke_case_sample_history(void);
+int smoke_case_gpu_metrics(void);
+int smoke_case_performance_navigation(void);
+int smoke_case_mountinfo(void);
+int smoke_case_storage_metadata(void);
+int smoke_case_filesystem_inventory(void);
+int smoke_case_bundled_pci(void);
+int smoke_case_smbios_memory(void);
+int smoke_case_system_sources(void);
+int smoke_case_process_model(void);
+int smoke_case_process_grouping(void);
+int smoke_case_process_gpu(void);
+int smoke_case_process_inspection(void);
+int smoke_case_process_management(void);
+int smoke_case_efficiency(void);
+int smoke_case_dbus_models(void);
+int smoke_case_preferences(void);
+int smoke_case_startup(void);
+int smoke_case_ui_update(void);
+int smoke_case_task_manager_layout(void);
+int smoke_case_hardware_topology(void);
+int smoke_case_intel_gpu(void);
+int smoke_case_npu_telemetry(void);
 
 #endif
