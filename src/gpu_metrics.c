29ae599f94100cc0219cf8fd3b40b29eafae20c2
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:26:41 +1000

    Standardise project copyright ownership (4/20)

--- src/gpu_metrics.c
+++ src/gpu_metrics.c
@@ -4,7 +4,7 @@
  * @brief Platform-neutral GPU metric selection for Performance graphs.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "gpu_metrics.h"
