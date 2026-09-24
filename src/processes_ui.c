67d737c62620ac67162b3f255570dea2ab06c447
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:54:33 +1000

    Standardise project copyright ownership (19/20)

--- src/processes_ui.c
+++ src/processes_ui.c
@@ -13,7 +13,7 @@
  * a recycled PID from a newer snapshot.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "processes_ui.h"
