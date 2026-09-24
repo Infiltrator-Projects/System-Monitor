10cfb6fe504a5d4782f82880225860acd0011082
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:52:29 +1000

    Standardise project copyright ownership (15/20)

--- src/process_table_ui.h
+++ src/process_table_ui.h
@@ -4,7 +4,7 @@
  * @brief Shared GTK table construction for process inspection tools.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_PROCESS_TABLE_UI_H
