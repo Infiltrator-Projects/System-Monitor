48d4f74f061b3bcf68f6fe1ca45ee2f957b343d3
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:51:08 +1000

    Standardise project copyright ownership (13/20)

--- src/sample_history.c
+++ src/sample_history.c
@@ -4,7 +4,7 @@
  * @brief Circular graph-history implementation.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "sample_history.h"
