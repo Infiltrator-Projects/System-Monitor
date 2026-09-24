0e3d07913d433e58029049048569cbe405dd99f1
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:48:57 +1000

    Standardise project copyright ownership (10/20)

--- src/mountinfo.h
+++ src/mountinfo.h
@@ -8,7 +8,7 @@
  * used to associate aliases, UUID mounts and bind mounts with sysfs devices.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_MOUNTINFO_H
