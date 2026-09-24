07c992a610c116ba444a1c08f90d2ea165cb3488
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:56:39 +1000

    Standardise support copyright ownership (22/22)

--- support/tools/windows_backend_probe.c
+++ support/tools/windows_backend_probe.c
@@ -9,7 +9,7 @@
  * layer are ported.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "monitor_platform.h"
