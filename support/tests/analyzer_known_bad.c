5355ae8492aaa4b9ff6c261d5d10e6dfb476a3af
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:28 +1000

    Standardise test copyright ownership (20/22)

--- support/tests/analyzer_known_bad.c
+++ support/tests/analyzer_known_bad.c
@@ -8,7 +8,7 @@
  * gate is not actually analysing.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include <stdlib.h>
