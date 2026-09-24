7150022f78190cbaf636208f49dbf83bacf78f08
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:56:03 +1000

    Standardise test copyright ownership (21/22)

--- support/tests/application_catalog_smoke.c
+++ support/tests/application_catalog_smoke.c
@@ -4,7 +4,7 @@
  * @brief Synthetic XDG desktop-application identity regression test.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #define _POSIX_C_SOURCE 200809L
