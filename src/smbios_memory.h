81e018c740376ba379502b87da821e8b2b05677a
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:50:17 +1000

    Standardise project copyright ownership (12/20)

--- src/smbios_memory.h
+++ src/smbios_memory.h
@@ -8,7 +8,7 @@
  * dependency on dmidecode while keeping the privileged surface very small.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_SMBIOS_MEMORY_H
