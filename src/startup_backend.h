c7f8ded1c52128f702af88045de935e20807a917
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:51:44 +1000

    Standardise project copyright ownership (14/20)

--- src/startup_backend.h
+++ src/startup_backend.h
@@ -7,7 +7,7 @@
  * semantics and durable writes remain in the selected platform backend.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_STARTUP_BACKEND_H
