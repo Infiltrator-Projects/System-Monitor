9b89e306fc750fab411228be548c8e95d8a7117b
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:52:57 +1000

    Standardise project copyright ownership (16/20)

--- src/performance_present.h
+++ src/performance_present.h
@@ -4,7 +4,7 @@
  * @brief Snapshot-to-widget presentation for Performance device pages.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_PERFORMANCE_PRESENT_H
