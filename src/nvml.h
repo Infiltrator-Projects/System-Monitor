99248de1df7c970bc2da47a9cb48ba79c7bf0fde
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:48:31 +1000

    Standardise project copyright ownership (10/20)

--- src/nvml.h
+++ src/nvml.h
@@ -4,7 +4,7 @@
  * @brief Optional native NVIDIA Management Library adapter.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_NVML_H
