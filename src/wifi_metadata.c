68a5b4665202ae2218c5b5238c11a3e81abbf08f
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:50:36 +1000

    Standardise project copyright ownership (13/20)

--- src/wifi_metadata.c
+++ src/wifi_metadata.c
@@ -14,7 +14,7 @@
  * or disconnected driver query cannot keep presenting stale link metadata.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "wifi_metadata.h"
