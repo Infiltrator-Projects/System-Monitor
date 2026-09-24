dc90ad083e91f0cc4f6339364a4beafb6354dfe6
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:41 +1000

    Standardise test copyright ownership (21/22)

--- support/tests/async_workers_smoke.c
+++ support/tests/async_workers_smoke.c
@@ -4,7 +4,7 @@
  * @brief Exercise detached process scanning and ordered recording workers.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "process_recorder.h"
