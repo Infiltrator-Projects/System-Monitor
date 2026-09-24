a0d169aeb537ca0e9211b94b531330841bf468aa
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:54:28 +1000

    Standardise project copyright ownership (19/20)

--- src/process_actions.c
+++ src/process_actions.c
@@ -14,7 +14,7 @@
  * redirecting a delayed menu action at an unrelated process.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "details_page.h"
