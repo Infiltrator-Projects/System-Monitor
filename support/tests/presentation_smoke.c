3adc226d7a3b5a5fbfb2a4e54849630d275fce70
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:38 +1000

    Standardise test copyright ownership (21/22)

--- support/tests/presentation_smoke.c
+++ support/tests/presentation_smoke.c
@@ -8,7 +8,7 @@
  * avoid either toolkit so architectural drift is caught at the shared boundary.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "performance_view.h"
