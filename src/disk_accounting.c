99383398bc4216617af9ca0e08b18d30d43e1520
Shannon Smith <infiltratr@yandex.com>
2026-09-24 12:58:40 +1000

    Standardise project copyright ownership (3/20)

--- src/disk_accounting.c
+++ src/disk_accounting.c
@@ -4,7 +4,7 @@
  * @brief Overflow-safe physical-disk rate, activity and latency accounting.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "disk_accounting.h"
--- src/filesystem_inventory.h
+++ src/filesystem_inventory.h
@@ -11,7 +11,7 @@
  * filesystem-policy responsibilities.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_FILESYSTEM_INVENTORY_H
