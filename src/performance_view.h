eecbc4bfcd79ecef1d61f8aa8b1591455f5e37ea
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:52:15 +1000

    Standardise project copyright ownership (15/20)

--- src/performance_view.h
+++ src/performance_view.h
@@ -9,7 +9,7 @@
  * value is available or how it is formatted.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_PERFORMANCE_VIEW_H
