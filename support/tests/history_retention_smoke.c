c01ee05482109d6b003d2e6c570087ae461253a7
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:56 +1000

    Standardise test copyright ownership (21/22)

--- support/tests/history_retention_smoke.c
+++ support/tests/history_retention_smoke.c
@@ -4,7 +4,7 @@
  * @brief Exercise bounded App History retention, persistence and live protection.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "app_internal.h"
