6678639b40494690ef547720a8a4610b11a6c681
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:54:17 +1000

    Standardise project copyright ownership (19/20)

--- src/performance_view.c
+++ src/performance_view.c
@@ -8,7 +8,7 @@
  * independently reinterpret telemetry availability or units.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "performance_view.h"
