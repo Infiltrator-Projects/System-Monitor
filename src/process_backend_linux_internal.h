89d460559e33596c93443991733d05c98d716487
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:53:54 +1000

    Standardise project copyright ownership (18/20)

--- src/process_backend_linux_internal.h
+++ src/process_backend_linux_internal.h
@@ -4,7 +4,7 @@
  * @brief Private Linux process-record parsing contracts used by implementation tests.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_PROCESS_BACKEND_LINUX_INTERNAL_H
