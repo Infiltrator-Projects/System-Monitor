15682a7a667acfabc16891b556dc8a21d426e888
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:15 +1000

    Standardise test copyright ownership (20/22)

--- support/tests/battery_smoke.c
+++ support/tests/battery_smoke.c
@@ -4,7 +4,7 @@
  * @brief Synthetic system and peripheral power-supply regression test.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "common.h"
