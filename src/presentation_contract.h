6726fa49f306f5a5e5a2b411383c958d197c834d
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:53:22 +1000

    Standardise project copyright ownership (17/20)

--- src/presentation_contract.h
+++ src/presentation_contract.h
@@ -10,7 +10,7 @@
  * This header deliberately contains no GTK, Win32 or operating-system types.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #ifndef INFILTRATOR_SYSTEM_MONITOR_PRESENTATION_CONTRACT_H
