e4c1f0ae6f95066d887cf7294284b0c5bb243a63
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:53:00 +1000

    Standardise project copyright ownership (16/20)

--- src/performance_present.c
+++ src/performance_present.c
@@ -9,7 +9,7 @@
  * only and must not perform hardware collection.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "performance_present.h"
