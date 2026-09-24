a4660d2757f0cca4988473a56a92cb0c925c733c
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:12 +1000

    Standardise test copyright ownership (20/22)

--- support/tests/backend_smoke.c
+++ support/tests/backend_smoke.c
@@ -3,7 +3,7 @@
  * @file backend_smoke.c
  * @brief Live native monitoring-backend smoke test.
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #include "monitor.h"
