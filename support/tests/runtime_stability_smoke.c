b6ea737f09be7b0176c2f48d5574aa8dbe60ec1e
Shannon Smith <infiltratr@yandex.com>
2026-09-24 14:55:59 +1000

    Standardise test copyright ownership (21/22)

--- support/tests/runtime_stability_smoke.c
+++ support/tests/runtime_stability_smoke.c
@@ -10,7 +10,7 @@
  * memory growth.
  *
  * @author Shannon Smith
- * @copyright Copyright (c) 2016-2026 Shannon Smith
+ * @copyright Copyright (c) 2000-2026 Shannon Smith
  * @license GPL-3.0-or-later
  */
 #define _POSIX_C_SOURCE 200809L
